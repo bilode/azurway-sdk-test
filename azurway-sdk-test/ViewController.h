feda2b4ad597cfa2370b9b6c3975b3262047308e
Timothée Bilodeau <timothee.bilodeau@gmail.com>
2016-06-30 19:29:57 +0200

    Initial Commit

---
+++ azurway-sdk-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  azurway-sdk-test
+//
+//  Created by Timothée Bilodeau on 30/06/16.
+//  Copyright © 2016 Timothée Bilodeau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ azurway-sdk-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  azurway-sdk-test
+//
+//  Created by Timothée Bilodeau on 30/06/16.
+//  Copyright © 2016 Timothée Bilodeau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
